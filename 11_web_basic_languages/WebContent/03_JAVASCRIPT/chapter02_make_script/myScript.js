c5d7067a47b65f742c77984fcf120e419a941a0c
12시_웹개발_yu <12__yu@DESKTOP-HB0IECH>
2021-02-25 14:47:56 +0900

    20210225

---
+++ 11_web_basic_languages/WebContent/03_JAVASCRIPT/chapter02_make_script/myScript.js
@@ -0,0 +1,16 @@
+
+alert("외부스크립트")
+
+document.write("<h1> Welcome to javascript</h1>");
+document.write("hello javascript1");
+document.write("hello javascript2");
+document.write("hello javascript3");
+document.write("<hr>");
+
+document.writeln("<h1> Welcome to javascript</h1>");
+document.writeln("<pre>")
+document.writeln("hello javascript1");
+document.writeln("hello javascript2");
+document.writeln("hello javascript3");
+document.writeln("</pre>")
+document.writeln("<hr>");
